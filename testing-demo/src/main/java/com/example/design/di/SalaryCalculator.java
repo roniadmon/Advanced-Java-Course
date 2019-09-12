4bd180f3bc2936924b69311d8fbf00d0d911c99e
orirabi <ori.rabi@segasec.com>
2019-09-12 14:42:14 +0300

    parametrized method example

--- testing-demo/src/main/java/com/example/design/di/SalaryCalculator.java
+++ testing-demo/src/main/java/com/example/design/di/SalaryCalculator.java
@@ -4,6 +4,9 @@ import java.time.Month;
 import java.time.ZonedDateTime;
 
 public class SalaryCalculator {
+
+    public static final int BONUS = 10000;
+
     public Double calc(Double total, Double percent, ZonedDateTime dateTime) {
         if (percent > 100) {
             percent = 100d;
@@ -13,7 +16,7 @@ public class SalaryCalculator {
         }
         double sum = total * percent / 100;
         if (Month.DECEMBER == dateTime.getMonth()) {
-            sum += 10000;
+            sum += BONUS;
         }
 
         return sum;
---
+++ testing-demo/src/test/java/com/example/design/di/SalaryCalculatorTest.java
@@ -0,0 +1,41 @@
+package com.example.design.di;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.stream.Stream;
+
+import static com.example.design.di.SalaryCalculator.BONUS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SalaryCalculatorTest {
+
+    @ParameterizedTest
+    @MethodSource("calcData")
+    void calc(double total, double percent, ZonedDateTime dateTime, double expected) {
+        SalaryCalculator salaryCalculator = new SalaryCalculator();
+        assertEquals(
+                expected,
+                salaryCalculator.calc(total, percent, dateTime)
+        );
+    }
+
+    private static Stream<Arguments> calcData() {
+        return Stream.of(
+                Arguments.of(100, 10, getZonedDateTimeOfMonth(11), 10),
+                Arguments.of(100, 10, getZonedDateTimeOfMonth(12), BONUS + 10),
+                Arguments.of(100, 200, getZonedDateTimeOfMonth(11), 100),
+                Arguments.of(100, 200, getZonedDateTimeOfMonth(12), BONUS + 100),
+                Arguments.of(100, -100, getZonedDateTimeOfMonth(11), 0),
+                Arguments.of(100, -100, getZonedDateTimeOfMonth(12), BONUS)
+        );
+    }
+
+    private static ZonedDateTime getZonedDateTimeOfMonth(int month) {
+        return ZonedDateTime.of(2019, month, 1, 1, 1, 1, 1, ZoneId.of("UTC"));
+    }
+
+}
