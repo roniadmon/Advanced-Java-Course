cc9bd30b009b55d34d1f00c78a0dba3c18c3a996
orirabi <ori.rabi@segasec.com>
2020-02-17 17:04:23 +0200

    transactionSaving

--- spring/4-data/src/main/java/com/mycompany/myapp/demo/DemoCourseService.java
+++ spring/4-data/src/main/java/com/mycompany/myapp/demo/DemoCourseService.java
@@ -30,7 +30,7 @@ public class DemoCourseService {
     public void demonstrate() {
         clean();
         log.info("Starting demo");
-        modifyInsideTransaction();
+        demoCRUD();
         System.exit(0);
     }
 
